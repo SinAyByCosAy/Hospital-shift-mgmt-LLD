8395a14b3bef924eb5ace68901b48c1d4460f84a
TanaySrivas <tanaysriofficial@gmail.com>
2023-05-12 14:05:09 +0530

    Staff can now have training and surgery

--- ClientChiefStaff.java
+++ ClientChiefStaff.java
@@ -1,9 +1,13 @@
 package Builder.HospitalShift;
 
-public class ClinetChiefStaff {
+public class ClientChiefStaff {
     public static void main(String args[]){
         StaffDirector sd = new StaffDirector(new DoctorBuilder());
         Staff staff = sd.getStaff();
+        System.out.println(staff.getStaffList());
+        staff.goTraining("Dr. Khan");
+        System.out.println(staff.getStaffList());
+        staff.goSurgery("Dr. Reddy");
         System.out.println(staff.getStaffList());
     }
 }
