4b56b56357326cc805dc5870a05f176dc57fae1b
TanaySrivas <tanaysriofficial@gmail.com>
2023-05-13 04:22:24 +0530

    Chief of Staff - Directly deal with Staff

--- ClientChiefStaff.java
+++ ClientChiefStaff.java
@@ -2,12 +2,19 @@ package Builder.HospitalShift;
 
 public class ClientChiefStaff {
     public static void main(String args[]){
+        //Communicating with Director to get details of doctors
         StaffDirector sd = new StaffDirector(new DoctorBuilder());
-        Staff staff = sd.getStaff();
-        System.out.println(staff.getStaffList());
-        staff.goTraining("Dr. Khan");
-        System.out.println(staff.getStaffList());
-        staff.goSurgery("Dr. Reddy");
-        System.out.println(staff.getStaffList());
+        Staff doc = sd.getStaff();
+        System.out.println(doc.getStaffList());
+        doc.goTraining("Dr. Khan");
+        System.out.println(doc.getStaffList());
+        doc.goSurgery("Dr. Reddy");
+        System.out.println(doc.getStaffList());
+
+        //Directly dealing with Nurses
+        StaffBuilder db = new NurseBuilder();
+        Staff nurse = db.setStartTime("10:00").setEndTime("20:00").setTimeLimit(10).setStaffList().build();
+        System.out.println(nurse.getStaffList());
+
     }
 }
