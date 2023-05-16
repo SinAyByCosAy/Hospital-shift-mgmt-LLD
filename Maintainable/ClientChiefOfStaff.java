3e0ce897d3132869ad5373eae643d967e2f40a3f
TanaySrivas <tanaysriofficial@gmail.com>
2023-05-16 05:50:21 +0530

    Testing Client

--- Maintainable/ClientChiefOfStaff.java
+++ Maintainable/ClientChiefOfStaff.java
@@ -1,4 +1,11 @@
 package Builder.HospitalShift.Maintainable;
 
 public class ClientChiefOfStaff {
+    public static void main(String args[]){
+        Staff staff = Staff.StaffBuilder.getInstance()
+                .setStartTime("9:00")
+                .setEndTime("21:00")
+                .build();
+        System.out.println(staff.startTime);
+    }
 }
